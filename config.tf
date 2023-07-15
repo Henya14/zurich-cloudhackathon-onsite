fcf7ce73fe3d0e5c46297a69d3d7a96852390ad9
Henrik Élő <henrikeloe@gmail.com>
2023-07-15 20:56:07 +0200

    Fix errors

--- config.tf
+++ config.tf
@@ -6,23 +6,42 @@ terraform {
         source = "hashicorp/aws"
     }
   }
+
 }
 
 provider "aws" {
   region = "us-east-1"
-}
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-
-locals {
-  ec2_key_pairs = {
-    "${var.ec2_instance1_name}_key" : {
-      public_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJZi8Hq/TRBFZxb0fFAovOpRMoN4rgeMjnJ420ygARYMxji1hh+nDLhMm6bf6Jro5cQxLDw7riUks5nNtzesxN5qAOqP8zr7PH9UtQCGhsPFyJiCwDnuVATCXdr9Xnjsfduw3zn54Z9YFZHBC97zobKZCI3TkharqfU4dRA4+A+8QxHxkjRgHAApKtotGX+pvA+A88NrrO3mXkInQujvkch0UEbYWZS76Lfhb+aivPZhFSHjGGZqyI5cj5i2ABJ0iRahRP2qLUuMqjClirG4QCjLLoE4Uywlx+5YAp7/5X+rFrqrQuv40Rwk7d3J5P3A+MD8ch/pO6ROE4n1xelCjMb4I6di8+0R20kmuVOi0Mvi05QgdB2sFrogzHlWC6Zia1CBghJmppQRH+LddS3sQqUp0PJhGXctKDBwwf/C7aCSkhtKMppt18sF+xRTL2CjvBRxplcJhM0Umvq0csJN0sWYLXl4A9QZSs+jlZNj294UiMcApp65mW81N8pIm1HShCKLxFf5/wrjPXe5scShA6oN37RT1SIKV6WfZm7W6VPpsbY1OyAQALNtVftZIo5jW+2Wy99UtMU0x10JzDE6q3YnLqEhTJRN4XsWt2olhF7rpyYa/54YB4W0+Q0yi20oa8kbJ00tT1mx8yAgsIXO5k5THCqN/kNAAV9DEIi3/hJw== root@DESKTOP-KBBVA00"
-    }
-    "${var.ec2_instance2_name}_key" : {
-      public_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2DY4TFnY4sGnCPaQpWAPEA7K+qNrNCDDEvq/AjmMbEFs1UPGziI6evU6qvX8mxw9EoVy4MNycdQ4bOct94pJp3JdHy31MsyXftO3Agd/h5+93B/PD6RLlCD+qGe0V6gU54jCQiIywpD7KUSVDxxwyNMSTjVTuHwG2mE631QMPLJMHLpNabxS7PzM1JQqFpMMNKlGJsfdXjR7dnvBh4G8ka4I6jG9bxNP8fdp3ZMZ1kC6VvkTHx6+TrJ+LqJqw0CuUyoHTRQeS4H5QunLu7hC04MYf6o6XFb754ezxYyFyxjSF40N+lI6q1WIJgfUw+8gWnNL8vAO9jCmEhz5IMdCiwol1wTcxuOV1VCKOZ1GP1G/gskFkEIJ+mXr9o1XIjC6OxjOPCGvaRZ8whZhnHJ/BLVdgw0fDcm2mKI5als3Vh5skOl9C87QF8T/yd68sSktWFzl6gr625CqiLPHj7PprJrDdIqrEpUnYyl2ErRRPguxlegHvXApuj0oOhSEXBLt5dPXYaCSC8eRFKd+iYm2ETQOMx5LxpOgi910yz3izwR63ah526OAIDtsU9XNBfWtzxRxFZRjg0zULYQ9VhY0jgAkv3TPs9hpXiVdi+VCVePuenLD0F0DLVPC5s/UUP/2TKck6KPhx91mZiM4bO+8KtBoDSMdnOgVBcA5mi+u4mQ== root@DESKTOP-KBBVA00"
-    }
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    apigatewayv2   = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
   }
 }
+
 
 
 
--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,13 @@
-resource "aws_instance" "ec2_instance1" {
-    ami = var.ami_id
-    instance_type = var.ec2_instance_type
-    security_groups = [
-        for sg in aws_security_group.ec2_security_group : sg.id
-    ]
-    tags = {
-        Name = "${var.ec2_instance1_name}"
-    }
-     key_name = aws_key_pair.ec2_instance_keypair["${var.ec2_instance1_name}_key"].key_name
-}   
+resource "aws_instance" "ec2_instances" {
+  for_each = var.ec2_instances
+  ami           = var.ami_id
+  instance_type = each.value.ec2_instance_type
+  security_groups = [
+    for sg in aws_security_group.ec2_security_group : sg.id
+  ]
+  tags = {
+    Name = "${each.key}"
+  }
+  key_name = aws_key_pair.ec2_instance_keypair["${each.key}"].key_name
+}
 
-resource "aws_instance" "ec2_instance2" {
-    ami = var.ami_id
-    instance_type = var.ec2_instance_type
-    security_groups = [
-        for sg in aws_security_group.ec2_security_group : sg.id
-    ]
-    tags = {
-        Name = "${var.ec2_instance1_name}"
-    }
-     key_name = aws_key_pair.ec2_instance_keypair["${var.ec2_instance2_name}_key"].key_name
-}   
